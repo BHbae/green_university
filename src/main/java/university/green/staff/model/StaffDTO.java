e83512c27d0dc8aa43a81a7fc4ff5ac61b7b6160
Sung-Hu <167273984+Sung-Hu@users.noreply.github.com>
2024-07-18 17:14:01 +0900

    이것저것...

--- src/main/java/university/green/staff/model/StaffDTO.java
+++ src/main/java/university/green/staff/model/StaffDTO.java
@@ -1,5 +1,25 @@
 package university.green.staff.model;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class StaffDTO {
 
+	private Integer id;
+	private String name;
+	private Date birthDate;
+	private String gender;
+	private String address;
+	private String tel;
+	private String email;
+	private Date hireDate;
 }
