790e8e258c4b2f0ae69d3c6a74d51e74fd30b41a
CYJ <wjdtpdbs1887@naver.com>
2024-07-18 17:48:10 +0900

    2024-07-18

--- src/main/java/university/green/student/controller/SugangController.java
+++ src/main/java/university/green/student/controller/SugangController.java
@@ -16,12 +16,11 @@ public class SugangController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
 	}
 
 }
