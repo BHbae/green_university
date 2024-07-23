cf2c25b4e720852105d03808b522cda727375b73
songhyun Eom <greenery3699@gmail.com>
2024-07-23 11:03:34 +0900

    merge stay

--- src/main/java/university/green/student/controller/StudentGradeController.java
+++ src/main/java/university/green/student/controller/StudentGradeController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet("/student-grade/*")
+@WebServlet("/studentGrade/*")
 public class StudentGradeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	// TODO - 사용할 DAO 생성하기
@@ -26,17 +26,24 @@ public class StudentGradeController extends HttpServlet {
 		// TODO - 유효성 체크 (직원이 맞는지, null값은 아닌지)
 		
 		switch(action) {
+		// 이번 학기 성적 조회
 		case "/grade-of-this-semester": {
 			selectThisSemester(request,response,session);
 			break;
 		}
+		// 학기별 성적 조회
 		case "/grade-select": {
 			selectEachSemester(request,response,session);
 			break;
 		}
+		// 총 성적 누계
 		case "/grade-total": {
 			selectTotalGrade(request,response,session);
 			break;
+		}
+		// 강의 평가 창
+		case "/show-evaluation": {
+			showSubjectEvaluation(request,response,session);
 		}
 		default: {
 			break;
@@ -44,6 +51,11 @@ public class StudentGradeController extends HttpServlet {
 		}
 	}
 
+
+	private void showSubjectEvaluation(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	private void selectTotalGrade(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/student/TotalAverageGrade.jsp").forward(request, response);
@@ -71,6 +83,9 @@ public class StudentGradeController extends HttpServlet {
 		case "/grade-select": {
 			// TODO - 학기별 성적 조회 
 			break;
+		}
+		case "/send-subject-evaluation":{
+			
 		}
 		default: {
 			break;
