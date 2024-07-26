9d7d68333376c76cd17ed6930bee52c393ec6e5f
CYJ <wjdtpdbs1887@naver.com>
2024-07-26 12:08:29 +0900

    merging

--- src/main/java/university/green/staff/repository/NoticeRepositoryImpl.java
+++ src/main/java/university/green/staff/repository/NoticeRepositoryImpl.java
@@ -19,16 +19,11 @@ public class NoticeRepositoryImpl implements NoticeRepository {
 	private static final String DELETE_NOTICE_SQL = "  DELETE FROM notice_tb WHERE id = ? ";
 	private static final String SELECT_ALL_NOTICE = " SELECT * FROM notice_tb limit ? offset ? ";
 	private static final String UPDATE_NOTICE_SQL = " UPDATE notice_tb SET title = ? , content = ?  WHERE id = ? ";
-<<<<<<< HEAD
 	
 	private static final String COUNT_NOTICE_SQL = " select count(*) as count from notice_tb; ";
 	
-=======
 	private static final String DATAL_NOTICE_SQL = " select * from notice_tb WHERE id = ? " ;
 
-	
-	private static final String COUNT_NOTICE_SQL = " select count(*) as count from notice_tb; ";
->>>>>>> 3e15b8919acd1850fd6894c5fc5895fcf9902ad6
 	private static final String SELECT_ALL_SCHEDULE = " select * from schedule_tb ";
 	private static final String INSERT_SCHEDULE = " INSERT INTO schedule_tb(staff_id,start_day,end_day,information) VALUES ( ? , ?, ?, ?) ";
 	
@@ -216,6 +211,7 @@ public class NoticeRepositoryImpl implements NoticeRepository {
 		return rowCount;
 	}
 
+	
 	@Override
 	public int countNotice() {
 		int count = 0;
@@ -233,52 +229,8 @@ public class NoticeRepositoryImpl implements NoticeRepository {
 		}
 		return count;
 	}
-	@Override
-	public int countNotice() {
-		int count = 0;
-		
-		try (Connection conn = DBUtil.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(COUNT_NOTICE_SQL)){
-			
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				count = rs.getInt("count");
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return count;
-	}
 
-	@Override
-	public List<ScheduleDTO> scheduleAll() {
-		List<ScheduleDTO> scheduleDTO = new ArrayList<>();;
-		
-		try (Connection conn = DBUtil.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_SCHEDULE)){
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				ScheduleDTO dto = new ScheduleDTO();
-				dto = ScheduleDTO.builder()
-						.id(rs.getInt("id"))
-						.staffId(rs.getInt("staff_id"))
-						.startDay(rs.getDate("start_day"))
-						.endDay(rs.getDate("end_day"))
-						.information(rs.getString("information"))
-						.build();
-				scheduleDTO.add(dto);
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return scheduleDTO;
-	}
-
-
+	
 	@Override
 	public int addSchedule(ScheduleDTO addaddSchedule, int staffId) {
 		int rowCount = 0;
@@ -335,41 +287,6 @@ public class NoticeRepositoryImpl implements NoticeRepository {
 		return scheduleDTO;
 	}
 
-
-	@Override
-	public int addSchedule(ScheduleDTO addaddSchedule, int staffId) {
-		int rowCount = 0;
-		
-		try (Connection conn = DBUtil.getConnection()){
-			conn.setAutoCommit(false);
-			try {
-				PreparedStatement pstmt = conn.prepareStatement(INSERT_SCHEDULE);
-				
-				pstmt.setInt(1, staffId);
-				pstmt.setDate(2, addaddSchedule.getStartDay());
-				pstmt.setDate(3, addaddSchedule.getEndDay());
-				pstmt.setString(4, addaddSchedule.getInformation());
-				
-				rowCount = pstmt.executeUpdate();
-				
-				conn.commit();
-				
-			} catch (Exception e) {
-				conn.rollback();
-				e.printStackTrace();
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return rowCount;
-	}
-
-	@Override
-	public List<NoticeDTO> getAllNotice() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public NoticeDTO detalNotice(int id) {
